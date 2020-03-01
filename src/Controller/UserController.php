7984c346260e855bbf8b00197577ea7f24b0231f
accessiproject <kevin.bustamante@mail.novancia.fr>
2020-03-01 15:36:39 +0100

    add user_edit in UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
 * @IsGranted("ROLE_ADMIN")
@@ -24,4 +29,39 @@ class UserController extends AbstractController
             'users' => $users,
         ]);
     }
+    
+    /**
+     * @Route("/edition-utilisateur/{id}", name="user_edit")
+     */
+    public function user_edit($id, Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $manager)
+    {
+
+        if ($id > 0)
+            $user = $manager->getRepository(User::class)->find($id);
+        else
+            $user = new User();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // Encode le mot de passe
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $user->setRoles(["ROLE_ADMIN"]);
+
+            // Enregistre le membre en base
+            $manager->persist($user);
+            $manager->flush();
+            
+            return $this->redirectToRoute('user_list');
+        }
+        return $this->render('user/edit.html.twig', [
+            'form' => $form->createView(),
+            'id' => $user->getId() ? $user->getId() : 0
+        ]);
+    }
+
 }
