22d3c92b2e0e02d34cb918f08488b679ef2dbcbf
accessiproject <kevin.bustamante@mail.novancia.fr>
2020-03-02 15:36:40 +0100

    creation Survey entity

---
+++ src/Entity/Survey.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SurveyRepository")
+ */
+class Survey
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=300)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=500)
+     */
+    private $question;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $multiple;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $closing_message;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdat;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedat;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $closedat;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getQuestion(): ?string
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(string $question): self
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    public function getMultiple(): ?bool
+    {
+        return $this->multiple;
+    }
+
+    public function setMultiple(bool $multiple): self
+    {
+        $this->multiple = $multiple;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getClosingMessage(): ?string
+    {
+        return $this->closing_message;
+    }
+
+    public function setClosingMessage(?string $closing_message): self
+    {
+        $this->closing_message = $closing_message;
+
+        return $this;
+    }
+
+    public function getCreatedat(): ?\DateTimeInterface
+    {
+        return $this->createdat;
+    }
+
+    public function setCreatedat(\DateTimeInterface $createdat): self
+    {
+        $this->createdat = $createdat;
+
+        return $this;
+    }
+
+    public function getUpdatedat(): ?\DateTimeInterface
+    {
+        return $this->updatedat;
+    }
+
+    public function setUpdatedat(\DateTimeInterface $updatedat): self
+    {
+        $this->updatedat = $updatedat;
+
+        return $this;
+    }
+
+    public function getClosedat(): ?\DateTimeInterface
+    {
+        return $this->closedat;
+    }
+
+    public function setClosedat(\DateTimeInterface $closedat): self
+    {
+        $this->closedat = $closedat;
+
+        return $this;
+    }
+
+    /**
+    * @ORM\PrePersist
+    * @ORM\PreUpdate
+    */
+    public function updatedTimestamps(): void {
+        $dateTimeNow = new \DateTime('now', new \DateTimeZone('Europe/Paris'));
+        $this->setUpdatedat($dateTimeNow);
+        if ($this->getCreatedat() === null) {
+            $this->setCreatedat($dateTimeNow);
+        }
+    }
+}
---
+++ src/Migrations/Version20200302143427.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200302143427 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE survey (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(300) NOT NULL, question VARCHAR(500) NOT NULL, multiple TINYINT(1) NOT NULL, status VARCHAR(255) NOT NULL, closing_message LONGTEXT DEFAULT NULL, createdat DATETIME NOT NULL, updatedat DATETIME NOT NULL, closedat DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, createdat DATETIME NOT NULL, updatedat DATETIME NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE survey');
+        $this->addSql('DROP TABLE user');
+    }
+}
