bb077077d458baf2c9c34596dbdd178ddfe0f547
accessiproject <kevin.bustamante@mail.novancia.fr>
2020-03-03 17:25:50 +0100

    add fields Survey Form Template

--- src/Controller/SurveyController.php
+++ src/Controller/SurveyController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Survey;
+use App\Form\SurveyType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,7 +27,32 @@ class SurveyController extends AbstractController
             'surveys' => $surveys,
         ]);
     }
-        /**
+
+    /**
+     * @Route("/edition/{id}", name="survey_edit")
+     */
+    public function survey_edit($id, Request $request, EntityManagerInterface $manager)
+    {
+        $survey = new Survey();
+        $form = $this->createForm(SurveyType::class, $survey);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $survey->updatedTimestamps();
+            $survey->setClosedat(new \DateTime('now'));
+
+            // Enregistre le sondage en base
+            $manager->persist($survey);
+            $manager->flush();
+            
+            return $this->redirectToRoute('survey_list');
+        }
+        return $this->render('survey/edit.html.twig', [
+            'controller_name' => 'SurveyController',
+            'form' => $form->createView(),
+        ]);
+    }
+    
+    /**
      * @Route("/consultation/{id}", name="survey_show")
      */
     public function survey_show($id)
---
+++ src/Form/SurveyType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Survey;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class SurveyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class,[
+                'label' => 'Titre du sondage :',
+            ])
+            ->add('question', TextareaType::class, [
+                'label' => 'Consigne donnée :'
+            ])
+            ->add('multiple', CheckboxType::class, [
+                'label' => 'Plusieurs réponses possibles.',
+                'required' => false,
+            ])
+            ->add('status', ChoiceType::class, [
+                'label' => 'Statut du sondage :',
+                'expanded' => true,
+                'choices' => [
+                    'Annulé' => 'Annulé',
+                    'Initialisé' => 'Initialisé',
+                    'Ouvert' => 'Ouvert',
+                    'Fermé' => 'Fermé',
+                ],
+                
+            ])
+            ->add('closing_message', TextareaType::class, [
+                'label' => 'Message de clôture du sondage :',
+                'attr' => array(
+                    'placeholder' => 'Le sondage est clôt.'
+                ),
+                'required' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Survey::class,
+        ]);
+    }
+}
