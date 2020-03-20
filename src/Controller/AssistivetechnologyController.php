8b316fca04a6d6e0f6c503117b761beec9ad6c90
accessiproject <kevin.bustamante@mail.novancia.fr>
2020-03-20 12:13:31 +0100

    creation Category Form

--- src/Controller/AssistivetechnologyController.php
+++ src/Controller/AssistivetechnologyController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,6 +24,32 @@ class AssistivetechnologyController extends AbstractController
         return $this->render('assistivetechnology/list.html.twig', [
             'controller_name' => 'AssistivetechnologyController',
             'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route("/edition/{id}", name="assistivetechnology_edit")
+     */
+    public function assistivetechnology_edit($id, Request $request, EntityManagerInterface $manager)
+    {
+        
+        if ($id > 0)
+            $category = $manager->getRepository(Category::class)->find($id);
+        else
+            $category = new Category();
+        
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Enregistre le sondage en base
+            $manager->persist($category);
+            $manager->flush();
+            
+            return $this->redirectToRoute('assistivetechnology_list');
+        }
+        return $this->render('assistivetechnology/edit.html.twig', [
+            'controller_name' => 'AssistivetechnologyController',
+            'form' => $form->createView(),
         ]);
     }
 
